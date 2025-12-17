8c0a651f02cb8bb80cc2477fa6a417bdccbc60c5
Alex Lacerda <windowfilm.br@gmail.com>
2025-12-17 11:27:42 +0000

    feat: pixel de 16 para 15 caacterie corrigido

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 // Simple Service Worker without Workbox CDN
 // PARA FORÇAR UMA NOVA VERSÃO: Altere o número da versão abaixo (ex: v71 -> v72)
-const CACHE_NAME = 'app-cache-v118';
+const CACHE_NAME = 'app-cache-v119';
 const urlsToCache = [
     '/',
     '/offline.html'
